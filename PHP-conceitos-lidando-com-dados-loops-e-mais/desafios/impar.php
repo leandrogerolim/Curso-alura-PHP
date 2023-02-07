631da91d5b89fdad120b879be6a548822c95498d
Leandro <leandrogerolim@gmail.com>
2023-02-07 14:51:19 -0400

    concluído desafios  e aulas.

---
+++ PHP-conceitos-lidando-com-dados-loops-e-mais/desafios/impar.php
@@ -0,0 +1,19 @@
+<?php
+$contador = 0;
+
+while($contador < 100){
+    $contador ++;
+    $valorFinal = $contador % 2;
+    if ($valorFinal == 0 ){
+       
+    }else{
+        echo "#$contador". PHP_EOL;
+    }
+        
+
+}
+    
+
+
+
+
